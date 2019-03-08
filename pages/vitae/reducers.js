edad790a2d6c9841590702943fddf0d52080a591
relubwu <bwu@ucsd.edu>
2019-03-08 14:34:00 -0800

    Append Reducers

---
+++ pages/vitae/reducers.js
@@ -0,0 +1,9 @@
+import { combineReducers } from '../../lib/redux.min';
+
+/**
+ * CUVita Client Side Implementations - reducers.js
+ * @scope /pages/vitae
+ * @author relubwu
+ * @version 0.1.5
+ * @copyright  © CHINESE UNION 2019
+ */
