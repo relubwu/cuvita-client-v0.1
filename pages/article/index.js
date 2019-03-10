0c7575af008ee53863d935ab662c642797eb748d
relubwu <bwu@ucsd.edu>
2019-03-10 14:38:55 -0700

    Append article page

---
+++ pages/article/index.js
@@ -0,0 +1,45 @@
+const app = getApp();
+const Store = app.store;
+
+/**
+ * CUVita Client Side Implementations - index.js
+ * @scope /pages/article
+ * @author relubwu
+ * @version 0.1.5
+ * @copyright  © CHINESE UNION 2019
+ */
+
+let imgPreviewList;
+const _generateImgPreviewList = (data) => {
+  let resultArray = [];
+  for (let i of data) {
+    if (i.nodeType == 'article__img' && i.previewable) {
+      resultArray.push(i.src);
+    }
+  }
+  return resultArray;
+}
+
+Page({
+  data: {
+    systemInfo: Store.getState().global.systemInfo
+  },
+  onLoad: function (options) {
+    let that = this;
+    app.request('/content/fetch', 'GET', { id: options.id }).then(res => {
+      this.setData(res);
+      imgPreviewList = _generateImgPreviewList(res.data);
+    });
+  },
+  onShareAppMessage: function () {
+
+  },
+  previewImage({ currentTarget: { dataset } }) {
+    if (dataset.previewable) {
+      wx.previewImage({
+        current: dataset.url,
+        urls: imgPreviewList
+      });
+    }
+  }
+})
