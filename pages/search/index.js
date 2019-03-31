ed9c574d5301342f48d76dd8507ebbb2b5136349
relubwu <bwu@ucsd.edu>
2019-03-30 17:07:10 -0700

    Minor update(s)

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,66 +1,21 @@
-// pages/search/index.js
-Page({
+const app = getApp();
+const { request } = app;
+const Store = app.store;
+const localepkg = require('./localepkg');
 
-  /**
-   * Page initial data
-   */
+/**
+ * CUVita Client Side Implementations - index.js
+ * @scope /pages/search
+ * @author relubwu
+ * @version 0.1.5
+ * @copyright  © CHINESE UNION 2019
+ */
+
+Page({
   data: {
 
   },
-
-  /**
-   * Lifecycle function--Called when page load
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page is initially rendered
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page show
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page hide
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page unload
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * Page event handler function--Called when user drop down
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * Called when page reach bottom
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * Called when user click on the top right corner to share
-   */
-  onShareAppMessage: function () {
+  onLoad(options) {
 
   }
 })
