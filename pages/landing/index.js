ba3137e8793b60d162624b77154886f8ed164a50
relubwu <bwu@ucsd.edu>
2019-04-01 00:04:01 -0700

    新增Landing页

---
+++ pages/landing/index.js
@@ -0,0 +1,36 @@
+const app = getApp();
+const { request } = app;
+const Store = app.store;
+const localepkg = require('./localepkg');
+
+/**
+ * CUVita Client Side Implementations - index.js
+ * @scope /pages/bind
+ * @author relubwu
+ * @version 0.1.5
+ * @copyright  © CHINESE UNION 2019
+ */
+
+const BIND_URL = '/pages/bind/index';
+
+Page({
+  data: {
+    systemInfo: Store.getState().global.systemInfo,
+    layout: ['https://cuvita-1254391499.cos.na-siliconvalley.myqcloud.com/landing/banner-discount.png', 'https://cuvita-1254391499.cos.na-siliconvalley.myqcloud.com/landing/banner-vip.png', 'https://cuvita-1254391499.cos.na-siliconvalley.myqcloud.com/landing/banner-gift.png'],
+    localepkg
+  },
+  onLoad(options) {
+    let that = this;
+    this.setData({
+      locale: Store.getState().global.locale
+    });
+  },
+  onBind() {
+    wx.navigateTo({
+      url: BIND_URL
+    });
+  },
+  onRegister() {
+    
+  }
+})
---
+++ pages/landing/localepkg.js
@@ -0,0 +1,12 @@
+module.exports = {
+  zh_CN: {
+    bind: "绑定CU卡",
+    register: "成为CU会员",
+    disclaimer: "绑定或注册代表您已阅读并同意隐私协议和用户协议"
+  },
+  en_US: {
+    bind: "Link",
+    register: "Register",
+    disclaimer: "Login or register represents your consent to our Privacy Policy and EULA"
+  }
+}
