9a0c656ab939ff5a9fff7d80d305debacddaef75
relubwu <bwu@ucsd.edu>
2019-04-13 12:10:31 -0700

    接机报名表功能追加

--- pages/arrival/pickup/index.js
+++ pages/arrival/pickup/index.js
@@ -8,13 +8,15 @@ const localepkg = require('./localepkg');
  * @scope /pages/arrival/pickup
  * @author relubwu
  * @version 0.1.5
- * @copyright  © CHINESE UNION 2019
+ * @copyright © CHINESE UNION 2019
  */
 
 Page({
   data: {
     systemInfo: Store.getState().global.systemInfo,
     currentStep: Store.getState().pages.arrival.pickup.currentStep,
+    minDate: Date.now(),
+    maxDate: new Date(2019, 9, 1).getTime(),
     localepkg
   },
   onLoad(options) {
@@ -41,5 +43,33 @@ Page({
   },
   onSubmit({ detail: { value: { cardno, name } } }) {
     let that = this;
+  },
+  explainFlight() {
+    wx.showModal({
+      title: localepkg[Store.getState().global.locale].flight,
+      content: localepkg[Store.getState().global.locale].flightDescription,
+      showCancel: !!0,
+      confirmColor: '#d1233e'
+    })
+  },
+  popupShow({ currentTarget: { dataset: { id } } }) {
+    this.setData({
+      [`popup.${ id }`]: !0
+    });
+  },
+  popupClose({ currentTarget: { dataset: { id } } }) {
+    this.setData({
+      [`popup.${ id }`]: !!0
+    });
+  },
+  departureDatePickerConfirm({ currentTarget: { dataset: { id } }, detail }) {
+    this.popupClose({ currentTarget: { dataset: { id: "departure" } } });
+    this.setData({
+      startDate: detail,
+      departure: new Date(detail).toLocaleDateString()
+    });
+  },
+  onSubmit(e) {
+    console.log(e);
   }
 })
